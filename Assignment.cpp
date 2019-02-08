a324b38d07cf34874b7c70d574f27b0ac5446a03
Cesar-Sepulveda <35970314+Cesar-Sepulveda@users.noreply.github.com>
2019-02-07 19:42:07 -0800

    Update Assignment.cpp

--- Assignment.cpp
+++ Assignment.cpp
@@ -2,20 +2,25 @@
 using namespace std;
 
 void reverseArray(int arr[] ,int size){
-  for (int i = 0, i < size, ++i){
-    arr[i] = arr[size - i];
+  int arrCopy[size];
+  for (int i = 0; i < size; ++i){
+    arrCopy[i] = arr[(size-1)-i];
+  }
+  for (int i = 0; i < size; i++){
+      arr[i] = arrCopy[i];
   }
 }
 
 int main(){
   int myArr[10];
-  for(int i = 0; i < 10, ++i){
+  for(int i = 0; i < 10; ++i){
     myArr[i] = i;
-    cout << myArr[i];
+    cout << myArr[i] << " ";
   }
-  reverseArray(myArr, 10)
-  for(int i = 0; i < 10, ++i){
-    cout << myArr[i];
+  cout << endl;
+  reverseArray(myArr, 10);
+  for(int i = 0; i < 10; ++i){
+    cout << myArr[i] << " ";
   }
   return 0;
 }
